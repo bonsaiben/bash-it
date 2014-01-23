9409690397362342d6c32a099635000be02533a7
Benjamin Sullivan <bsullivan2@gmail.com>
2014-01-24 00:09:43 +0900

    My rails-related aliases

--- aliases/available/rails.aliases.bash
+++ aliases/available/rails.aliases.bash
@@ -11,7 +11,7 @@ alias rd='rails dbconsole'
 alias rp='rails plugin'
 alias ra='rails application'
 alias rd='rails destroy'
-alias dbm='rake db:migrate'
+alias dbm='rake db:migrate; rake db:migrate RAILS_ENV=test'
 
 alias ss='script/server'
 alias ts="thin start"     # thin server
@@ -20,3 +20,32 @@ alias restartapp='touch tmp/restart.txt'
 alias restart='touch tmp/restart.txt'  # restart passenger
 alias devlog='tail -f log/development.log'
 alias taild='tail -f log/development.log' # tail dev log
+alias fs='foreman start'
+alias t='be rspec'
+alias tsp='be rspec spec'
+
+function grv(){
+  grep -r $* app/views --color
+}
+function grc(){
+  grep -r $* app/controllers --color
+}
+function grm(){
+  grep -r $* app/models --color
+}
+function gra(){
+  grep -r $* app/assets --color
+}
+function grs(){
+  grep -r $* spec --color
+}
+function grc(){
+  grep -r $* config --color
+}
+function grm(){
+  grep -r $* lib --color
+}
+function grr() {
+  grep -r $* app lib config db spec --color
+}
+
